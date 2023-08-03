e1bdfef69de2f26b1ceb2f56911229c5cb6d9137
Chinmay <chinmaym49@gmail.com>
2023-08-03 22:09:29 +0530

    arrays

---
+++ 13array.ts
@@ -0,0 +1,10 @@
+let arr1 : number[] = [1,2,3];
+let arr2 : Array<string> = ["cef","rukg","keurh"];
+let arr3 : number[][] = [
+    [1,2,3],
+    [4,5,6]
+];
+
+console.log(arr1, arr2, arr3);
+
+export {};
