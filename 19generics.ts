cedd08144cbc0bf6ced3df9dc17d3e24b8b007da
Chinmay <chinmaym49@gmail.com>
2023-08-13 17:58:03 +0530

    generic arrow function

--- 19generics.ts
+++ 19generics.ts
@@ -1,7 +1,12 @@
-function foo<Type>(arg : Type) : Type {
+// used comma to tell developer that this is not a jsx tag.
+function foo<Type,>(arg : Type) : Type {
     return arg;
+}
+
+let fooo = <Type,>(args : Type) : Type => {
+    return args;
 }
 
 console.log(typeof foo("clwnc"));
 console.log(typeof foo(242));
-console.log(typeof foo({x : 1, y : 2}));
+console.log(typeof fooo({x : 1, y : 2}));
